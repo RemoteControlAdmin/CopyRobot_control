560923b80bab8bf6ed0de8ca55c37d60fbe32361
ArusTaimika <arus2.maru2@gmail.com>
2025-03-24 09:51:57 +0900

    invの修正(定義の差異)

--- include/inverce_kinematics.hpp
+++ include/inverce_kinematics.hpp
@@ -17,9 +17,9 @@ namespace robotkinematics {
 
     Eigen::Vector3d invrobot_velocity_cal(Eigen::Matrix3d inverse_trans, Eigen::Vector3d velocity_data);
     
-    Eigen::Matrix3d invmatrix_cal(Eigen::Matrix3d inverse);
+    Eigen::Matrix3d invmatrix_cal();
 
-    Eigen::Vector3d invwheel_velocity_cal(Eigen::Matrix3d inverse_trans, Eigen::Vector3d velocity_data, Eigen::Vector3d invrobotvelocity);
+    Eigen::Vector3d invwheel_velocity_cal(Eigen::Vector3d velocity_data, Eigen::Vector3d invrobotvelocity);
 
     private:
     Eigen::Matrix3d inverse_trans;
