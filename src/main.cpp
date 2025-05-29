105e172b131253ae96c7561b600ae44082ad9380
ArusTaimika <arus2.maru2@gmail.com>
2025-05-29 14:12:24 +0900

    force data 表示

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,10 @@ void show_data(RobotData robotdata, Eigen::Vector3d velocity_data, int dt){
               << std::left << std::setw(20) << ("Vy = " + std::to_string(velocity_data[1]))
               << std::left << std::setw(20) << ("Vt = " + std::to_string(velocity_data[2]))
               << std::endl;*/
+    std::cout << std::left << std::setw(20) << ("FEactM = " + std::to_string(robotdata.force_actual_data[0])) 
+              << std::left << std::setw(20) << ("FEVir = " + std::to_string(robotdata.force_virtual_data.first[5]))
+              << std::left << std::setw(20) << ("F = " + std::to_string(robotdata.force_ideal_data.first[5]))
+              << std::endl;
     std::cout << std::left << std::setw(20) << ("dt = " + std::to_string(dt)) 
               << std::endl;
     std::cout << "==============================================" << std::endl;
