5b32bb80ad20e54091c6535d7b08eff479d338c6
ArusTaimika <arus2.maru2@gmail.com>
2025-05-30 19:40:51 +0900

    err修正

--- src/main.cpp
+++ src/main.cpp
@@ -156,7 +156,7 @@ int main(){
         std::tuple <std::vector<double>, std::vector<double>, std::vector<double>> temp_convert_data 
             = robot_data_cal.convert_robotdata(robotdata.master_data, robotdata.copy_data, robotdata.partner_master_data);    //Get MasterRobot's Position for manual path trajectory
         //robotdata.master_data = robot_data_cal.MRobot_Linear_PositionCal(robotdata.master_data, (micro_current_clock - first_clock).count());
-        robotdata.master_data = temp_convert_data.first;  robotdata.copy_data = temp_convert_data.second; robotdata.partner_master_data = temp_convert_data.third;
+        robotdata.master_data = std::get<0>(temp_convert_data); robotdata.copy_data = std::get<1>(temp_convert_data); robotdata.partner_master_data = std::get<2>(temp_convert_data);
         robotdata.err_data = robot_data_cal.err_robotposition_cal(robotdata.master_data, robotdata.copy_data);
         
         /*
